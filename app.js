f078da83a9aa93df93906ffd942f871e76d6307f
Inna Sofronyuk <inn-okey@yandex.ru>
2022-08-25 15:01:44 +0300

    ffff

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const { errors } = require('celebrate');
-const { handleError } = require('./middlewares/handleError');
+const handleError = require('./middlewares/handleError');
 const router = require('./routes/index');
 
 const { PORT = 3000 } = process.env;
@@ -13,12 +13,13 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(router);
+
 app.use(errors()); // ошибки celebrate
+app.use(handleError); // центральная обработка ошибок
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
   useUnifiedTopology: false,
 });
 
-app.use(handleError); // центральная обработка ошибок
 app.listen(PORT);
